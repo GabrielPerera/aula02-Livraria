c99e6bb5d875bd4eea7ad4088ed7306ddf9e54c1
GabrielPerera <gabrielpereira0907@gmail.com>
2024-01-15 14:01:37 -0300

    Questão da livraria

---
+++ Livraria.java
@@ -0,0 +1,14 @@
+public class Livraria {
+    public static void main(String[] args) {
+        Livro livro1 = new Livro();
+
+        livro1.anoPublicacao = 2024;
+        livro1.autor = "irineu";
+        livro1.titulo = "não sei";
+
+        Livro livro2 = new Livro("DragonBall", "Akira", 1996);
+
+
+
+    }
+}
---
+++ Livro.java
@@ -0,0 +1,14 @@
+public class Livro {
+    public String titulo;
+    public String autor;
+    public int anoPublicacao;
+
+    public Livro(String titulo, String autor, int anoPublicacao){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public Livro(){
+    }
+}
